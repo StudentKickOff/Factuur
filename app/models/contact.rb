2a3c1c3d2103e141d37cc62707f883581e0a481d
Rien Maertens <maertensrien@gmail.com>
2017-07-17 17:01:20 +0200

    Add vatnumber + beneficiary

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,8 +6,9 @@ class Contact
   has_many :invoices
 
   field :name, type: String
+  field :vatnumber, type: String
 
-  validates_presence_of :name
+  validates_presence_of :name, :vatnumber
 
   def to_s
     name
@@ -18,12 +19,13 @@ end
 class Address
   include Mongoid::Document
 
+  field :beneficiary, type: String
   field :street, type: String
   field :zip_code, type: String
   field :city, type: String
   field :country, type: String
 
-  validates :street, :zip_code, :city, :country, presence: true
+  validates_presence_of :street, :zip_code, :city, :country
 
   embedded_in :contact
 end
