619d97a35e410dde65292eb040dde52e9c0b339a
Rien Maertens <maertensrien@gmail.com>
2017-07-17 15:22:40 +0200

    Translations & validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,9 +7,12 @@ class Contact
 
   field :name, type: String
 
+  validates_presence_of :name
+
   def to_s
     name
   end
+
 end
 
 class Address
@@ -19,6 +22,8 @@ class Address
   field :zip_code, type: String
   field :city, type: String
   field :country, type: String
+
+  validates :street, :zip_code, :city, :country, presence: true
 
   embedded_in :contact
 end
